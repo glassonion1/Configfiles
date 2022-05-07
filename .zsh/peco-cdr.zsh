15b0572bef90063a9af4d9e3c2de54fa20c0372a
glassonion1 <taisuke.fujita@layerx.co.jp>
2022-05-07 18:54:17 +0900

    adds peco codes

---
+++ .zsh/peco-cdr.zsh
@@ -0,0 +1,12 @@
+function peco-cdr() {
+  local selected_dir=$(cdr -l | awk '{ print $2 }' | peco)
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+
+zle -N peco-cdr
+# 履歴から cd
+bindkey '^x' peco-cdr
